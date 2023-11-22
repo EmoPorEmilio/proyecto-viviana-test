37dd574c86e7e4a39309e56ca51cd2d53e60b033
Emilio <franceschini.emilio@gmail.com>
2023-11-22 18:03:28 -0300

    update color

--- components/Lateral-Nav/index.tsx
+++ components/Lateral-Nav/index.tsx
@@ -2,7 +2,7 @@ import Item from './Item';
 
 const LateralNav = () => {
   return (
-    <div className='w-[300px] bg-dark-bg border-r border-primary-300 p-3 m-0'>
+    <div className='w-[300px] bg-dark-bg border-r border-primary-600 p-3 m-0'>
       <Item title='Comenzando'></Item>
     </div>
   );
