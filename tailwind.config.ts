a567ec87207d5be3f39d4dfe0bb5cc5a10a88ec0
Emilio <franceschini.emilio@gmail.com>
2023-11-22 17:56:21 -0300

    author or date

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,9 +15,10 @@ const config: Config = {
       white: '#FFFFFF',
       'primary-100': '#D9F2FF',
       'primary-200': '#D0E9F5',
-      'primary-300': '#58748B',
-      'primary-400': '#4F6D85',
-      'primary-500': '#4C6477',
+      'primary-400': '#ADCCDC',
+      'primary-600': '#58748B',
+      'primary-700': '#4F6D85',
+      'primary-800': '#4C6477',
     },
     extend: {
       dropShadow: {
