0879516aebadbe3075ead53c65b442eec0cb0538
Emilio <franceschini.emilio@gmail.com>
2023-11-24 17:58:23 -0300

    change color

--- components/Dialog/index.tsx
+++ components/Dialog/index.tsx
@@ -7,7 +7,7 @@ const Dialog = () => (
     <div className='flex flex-col w-[500px] justify-end h-[50px] rounded-t-[26px] bg-primary-800 '>
       <div className='flex flex-col justify-end h-[40px] rounded-t-[30px] bg-primary-600'>
         <div className='flex items-center justify-end h-[20px] rounded-t-[100px] bg-primary-700 pr-4'>
-          <span className='text-primary-400 text-base font-regular'>
+          <span className='text-primary-300 text-base font-regular'>
             Emo - 16/4/22
           </span>
         </div>
