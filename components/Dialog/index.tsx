7f2b2503801601481c06270d0aafaf77c6aa1e95
Emilio <franceschini.emilio@gmail.com>
2023-11-24 22:39:48 -0300

    responsive

--- components/Dialog/index.tsx
+++ components/Dialog/index.tsx
@@ -3,11 +3,11 @@ import { Button, ButtonType } from '@/components/Button';
 import { Chip, ChipType } from '@/components/Chip';
 
 const Dialog = () => (
-  <div className='flex flex-col w-[540px] h-[350px]'>
-    <span className='relative top-4 left-10 text-4xl font-extrabold text-transparent	bg-clip-text bg-gradient-to-b from-primary-100 to-primary-500'>
+  <div className='flex flex-col md:w-[540px]w-[540px] h-[350px]'>
+    <span className='relative top-4 left-8 text-4xl font-extrabold text-transparent	bg-clip-text bg-gradient-to-b from-primary-100 to-primary-500'>
       main.rs
     </span>
-    <div className='flex flex-col w-[500px] justify-end h-[50px] rounded-t-[26px] bg-primary-800 '>
+    <div className='flex flex-col md:w-[500px] justify-end h-[50px] rounded-t-[26px] bg-primary-800 '>
       <div className='flex flex-col justify-end h-[40px] rounded-t-[30px] bg-primary-600'>
         <div className='flex items-center justify-end h-[20px] rounded-t-[100px] bg-primary-700 pr-4'>
           <span className='text-primary-300 text-base font-regular'>
@@ -16,7 +16,7 @@ const Dialog = () => (
         </div>
       </div>
     </div>
-    <div className='flex flex-col mt-1 w-[500px] h-[300px] rounded-t-lg rounded-b-[26px] bg-darkest-bg border-accent-500 border-solid border-b'>
+    <div className='flex flex-col mt-1 md:w-[500px] h-[300px] rounded-t-lg rounded-b-[26px] bg-darkest-bg border-accent-500 border-solid border-b'>
       <div className='flex flex-col flex-1 p-4'>
         <span className={`${sen.className} text-gray-100 text-lg`}>
           Esto es un test de texto! Esto es un test de texto!
