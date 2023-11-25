c16153a2ee6c6131260b7d14ad094ab61d4594aa
Emilio <franceschini.emilio@gmail.com>
2023-11-25 10:20:47 -0300

    fix icon size

--- app/icons/Icon.tsx
+++ app/icons/Icon.tsx
@@ -1,5 +1,5 @@
 const HamburgerMenuIcon = ({ ...props }) => (
-  <svg {...props} viewBox='0 0 31 21' fill='none'>
+  <svg {...props} width='31' height='21' viewBox='0 0 31 21' fill='none'>
     <path
       d='M0.125 20.75H30.875V17.3333H0.125V20.75ZM0.125 12.2083H30.875V8.79167H0.125V12.2083ZM0.125 0.25V3.66667H30.875V0.25H0.125Z'
       fill='#75ABC7'
