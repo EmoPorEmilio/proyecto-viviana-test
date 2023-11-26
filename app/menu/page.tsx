26ce747c6db018e34a11903d768efdbc692763fd
Emilio <franceschini.emilio@gmail.com>
2023-11-26 16:32:18 -0300

    add menu

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,32 +1,33 @@
 import { sen } from '@/app/fonts';
+import Link from 'next/link';
 
 export default function Menu() {
   return (
-    <div className='flex flex-col h-full w-full h-[100dvh] w-[100dvw]'>
+    <div className='flex flex-col h-[100dvh] w-[100dvw]'>
       <div className='w-full h-14 bg-bg-400 flex justify-center items-center border-b-4 border-accent-500 border-solid'>
         <span className='text-4xl font-bold tracking-wider text-primary-300'>
           ANUNCIOS
         </span>
       </div>
-      <div className='w-full h-10 bg-primary-700 border-b-2 border-bg-100 flex items-center pl-4'>
-        <span className={`${sen.className} text-xl text-gray-200`}>
+      <div className='w-full h-10 bg-primary-600 border-b-4 border-bg-100 flex items-center pl-4'>
+        <Link href='/' className={`${sen.className} text-xl text-gray-200`}>
           Proyecto Viviana
-        </span>
+        </Link>
       </div>
-      <div className='w-full h-10 bg-primary-700 border-b-2 border-bg-100 flex items-center pl-4'>
-        <span className={`${sen.className} text-xl text-gray-200`}>
+      <div className='w-full h-10 bg-primary-600 border-b-4 border-bg-100 flex items-center pl-4'>
+        <Link href='/' className={`${sen.className} text-xl text-gray-200`}>
           Proyecto Viviana
-        </span>
+        </Link>
       </div>
-      <div className='w-full h-10 bg-primary-700 border-b-2 border-bg-100 flex items-center pl-4'>
-        <span className={`${sen.className} text-xl text-gray-200`}>
+      <div className='w-full h-10 bg-primary-600 border-b-4 border-bg-100 flex items-center pl-4'>
+        <Link href='/' className={`${sen.className} text-xl text-gray-200`}>
           Proyecto Viviana
-        </span>
+        </Link>
       </div>
-      <div className='w-full h-10 bg-primary-700 border-b-2 border-bg-100 flex items-center pl-4'>
-        <span className={`${sen.className} text-xl text-gray-200`}>
+      <div className='w-full h-10 bg-primary-600 border-b-4 border-bg-100 flex items-center pl-4'>
+        <Link href='/' className={`${sen.className} text-xl text-gray-200`}>
           Proyecto Viviana
-        </span>
+        </Link>
       </div>
       <div className='w-full h-14 bg-bg-400 flex justify-center items-center border-b-4 border-accent-500 border-solid'>
         <span className='text-4xl font-bold tracking-wider text-primary-300'>
