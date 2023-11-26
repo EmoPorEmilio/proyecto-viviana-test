bfa7a3a0e362cc038eb39936480ebf9ce6f5e42a
Emilio <franceschini.emilio@gmail.com>
2023-11-26 15:55:20 -0300

    padding header finished

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,7 +2,7 @@ import HamburgerMenuIcon from '@/app/icons/Icon';
 
 const Header = () => (
   <nav className='flex items-center bg-bg-400 h-[70px] border-bottom border-solid border-b-4 border-accent-500'>
-    <div className='md:block hidden pl-2'>
+    <div className='md:block hidden pl-8'>
       <HamburgerMenuIcon className='' size={16} />
     </div>
     <h1 className='pl-8 text-primary-100 text-[34px] subpixel-antialiased font-medium drop-shadow-title'>
