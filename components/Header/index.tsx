eb3dd710668e08e821f8278a50a7f222aee8087c
Emilio <franceschini.emilio@gmail.com>
2023-11-26 15:40:24 -0300

    changes in header paddings

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,10 +2,16 @@ import HamburgerMenuIcon from '@/app/icons/Icon';
 
 const Header = () => (
   <nav className='flex items-center bg-bg-400 h-[70px] border-bottom border-solid border-b-4 border-accent-500'>
-    <HamburgerMenuIcon size={16} />
-    <h1 className='text-primary-100 text-[34px] subpixel-antialiased font-medium drop-shadow-title'>
+    <div className='md:block hidden pl-2'>
+      <HamburgerMenuIcon className='' size={16} />
+    </div>
+    <h1 className='pl-8 text-primary-100 text-[34px] subpixel-antialiased font-medium drop-shadow-title'>
       Tutorial Axum
     </h1>
+
+    <div className='md:hidden flex flex-1 justify-end pr-8'>
+      <HamburgerMenuIcon size={16} />
+    </div>
   </nav>
 );
 
