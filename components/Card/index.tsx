4edeb8439cae2ff955731e2338e3ac2a2fd34706
Emilio <franceschini.emilio@gmail.com>
2023-12-01 16:11:02 -0300

    fire

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,47 +1,54 @@
-import Image from 'next/image';
-import EventImage from './event.jpg';
-import { Chip, ChipType } from '@/components/Chip';
-import { MdOutlineAccessTime, MdAlternateEmail } from 'react-icons/md';
-import { HiFire } from 'react-icons/hi';
+import Image from "next/image";
+import EventImage from "./event.jpg";
+import FireImage from "./fire.png";
+import { MdOutlineAccessTime, MdAlternateEmail } from "react-icons/md";
 
 export const Card = () => (
-  <div className='flex flex-col justify-center items-center w-[240px] h-[250px] rounded-3xl border-b border-accent-500'>
-    <div className='w-full h-full rounded-t-3xl'>
-      <div className='relative border-accent-200 w-full h-full bg-white rounded-t-3xl'>
-        <div className='z-10 w-9 absolute right-[-15px] bg-opacity-20 h-full'>
-          <div className='flex flex-col w-full h-full items-center'>
-            <HiFire className='text-red-600' size='40px' />
+  <div className="flex h-[250px] w-[240px] flex-col items-center justify-center rounded-3xl border-b border-accent-500">
+    <div className="h-full w-full rounded-t-3xl">
+      <div className="relative h-full w-full rounded-t-3xl border-accent-200 bg-white">
+        <div className="absolute right-[-15px] z-20 h-full w-9 bg-opacity-20">
+          <div className="flex h-full w-full flex-col items-center">
+            <div className="relative h-12 w-12">
+              <Image src={FireImage} alt="fire" fill={true} />
+            </div>
+            <div className="relative h-12 w-12">
+              <Image src={FireImage} alt="fire" fill={true} />
+            </div>
+            <div className="relative h-12 w-12">
+              <Image src={FireImage} alt="fire" fill={true} />
+            </div>
           </div>
         </div>
-        <div className=' z-10 absolute bottom-0 left-0 w-[240px] h-full bg-gradient-to-b from-transparent from-20% to-90% to-bg-400'>
-          <div className='flex w-full h-full flex-col items-center justify-end px-3 pb-2'>
-            <span className='text-white font-bold text-xl drop-shadow-title-card text-center'>
+        <div className=" absolute bottom-0 left-0 z-10 h-full w-[240px] bg-gradient-to-b from-transparent from-20% to-bg-400 to-90%">
+          <div className="flex h-full w-full flex-col items-center justify-end px-3 pb-2">
+            <span className="text-center text-xl font-bold text-white drop-shadow-title-card">
               Fiesta de Taylor 2021 todos invitados
             </span>
           </div>
         </div>
         <Image
-          alt='event'
+          alt="event"
           src={EventImage}
           fill={true}
-          style={{ objectFit: 'cover', borderRadius: '24px' }}
+          style={{ objectFit: "cover", borderRadius: "24px" }}
         />
       </div>
     </div>
-    <div className='flex border-t border-primary-800 border-opacity-30 w-full flex-col items-center self-center justify-center h-[46px] bg-bg-400 rounded-b-3xl'>
-      <div className='flex flex-1 justify-center gap-8 items-center'>
-        <div className='flex w-full h-full flex-1 justify-end items-center gap-1'>
-          <div className='flex items-center gap-[2px]'>
-            <MdAlternateEmail className='text-primary-700' />
+    <div className="flex h-[46px] w-full flex-col items-center justify-center self-center rounded-b-3xl border-t border-primary-800 border-opacity-30 bg-bg-400">
+      <div className="flex flex-1 items-center justify-center gap-8">
+        <div className="flex h-full w-full flex-1 items-center justify-end gap-1">
+          <div className="flex items-center gap-[2px]">
+            <MdAlternateEmail className="text-primary-700" />
             &nbsp;
-            <span className='font-semibold text-accent-200 '>taylorswift</span>
+            <span className="font-semibold text-accent-200 ">taylorswift</span>
           </div>
         </div>
-        <div className='flex w-full h-full flex-1 justify-start items-center '>
-          <div className='flex items-center gap-[2px]'>
-            <MdOutlineAccessTime className='text-primary-700' />
+        <div className="flex h-full w-full flex-1 items-center justify-start ">
+          <div className="flex items-center gap-[2px]">
+            <MdOutlineAccessTime className="text-primary-700" />
             &nbsp;
-            <span className='font-semibold text-primary-300 '>
+            <span className="font-semibold text-primary-300 ">
               <b>2</b> días
             </span>
           </div>
